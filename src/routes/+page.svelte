4206a1ed13a65e1e13708e4e65f4a4c982ba9701
Marco <marco@yeboster.com>
2023-08-18 13:33:06 +0200

    feat: update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero-primary">
+	<div class="content">
+		<h1>Meta Names</h1>
+		<h5>The only name you need</h5>
+	</div>
+</div>
+<div class="hero-secondary">
+	<div class="content">
+		<h2>The official Partisia Naming System</h2>
+		<h5>Decentralised naming for wallets, social, websites</h5>
+	</div>
+</div>
+
+<style>
+	.content {
+		max-width: 1280px;
+		margin: 0 auto;
+		padding: 8rem;
+		text-align: center;
+
+		@media only screen and (max-width: 600px) {
+			padding: 10%;
+		}
+	}
+
+	.hero-primary {
+		color: var(--mdc-theme-text-primary-on-dark);
+		background-color: var(--mdc-theme-primary);
+	}
+
+	.hero-secondary {
+		background-color: var(--mdc-theme-background);
+	}
+</style>
