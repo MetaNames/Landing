7a468c6e9aa41c26aee23b13204d4463e00b236f
Marco <marco@yeboster.com>
2023-08-21 17:37:30 +0200

    fix: build

--- src/routes/OpenAppButton.svelte
+++ src/routes/OpenAppButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Button, { Icon } from '@smui/button';
 
-	import { metaNamesUrl} from '$lib';
+	import { metaNamesAppUrl } from '$lib';
 
 	import '../styles/app.scss';
 
@@ -12,7 +12,7 @@
 	const iconClasses = variant === 'text' ? ' icon-on-primary' : '';
 </script>
 
-<Button class={klass} {color} {variant} href="{metaNamesUrl}" target="_blank">
+<Button class={klass} {color} {variant} href="{metaNamesAppUrl}" target="_blank">
 	<Icon class="material-icons {iconClasses}">open_in_new</Icon>
 	Open App
 </Button>
