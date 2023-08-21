5cf36a7a9776ef5cad100fb9d7444b23fcda3cb6
Marco <marco@yeboster.com>
2023-08-21 21:47:02 +0200

    fix(env): app url

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,1 @@
-let metaNamesUrl = `${process.env.META_NAMES_APP_URL}`
-if (metaNamesUrl.length === 0) metaNamesUrl = 'https://app.metanames.io';
-
-export const metaNamesAppUrl = metaNamesUrl;
+export const metaNamesAppUrl = `${import.meta.env.VITE_META_NAMES_APP_URL}`
