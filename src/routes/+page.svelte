01e5c23b92c4e1dd026835a56b364721379722d8
Marco <marco@yeboster.com>
2023-08-25 13:43:13 +0200

    refactor: homepage text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,30 @@
 
 <div class="hero-primary">
 	<div class="content">
-		<h2>The official Name System for Partisia Blockchain</h2>
+		<h1>Meta Names</h1>
 		<Typewriter>
-			<h5>The decentralised naming system for wallets, social, websites, and more!</h5>
+			<h5>The official Name System for Partisia Blockchain</h5>
 		</Typewriter>
 		<OpenAppButton color="primary" klass="btn-primary-on-hero" variant="outlined" />
 	</div>
 </div>
+
 <div class="hero-secondary">
+	<div class="content">
+		<h2>The only name you need for seamless experiences</h2>
+		<h5>
+			Register your domain and subdomains effortlessly with Meta Names, the cutting-edge domain name
+			system for <a href="https://partisiablockchain.com/" target="_blank" rel="noopener noreferrer"
+				>Partisia Blockchain</a
+			>.
+			<br />
+			Unlock a world of possibilities by conveniently saving various information within each domain:
+			manage wallet addresses, social handles, website links, and more.
+		</h5>
+	</div>
+</div>
+
+<div class="hero-primary">
 	<div class="content">
 		<h2>Generate your name</h2>
 		<p>
