9deca8c3e7a620a396cda1250c439bb655df5bc2
Marco <marco@yeboster.com>
2023-11-11 17:56:51 +0100

    feat: improve footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,13 +1,37 @@
 <script>
+	import Logo from "./Logo.svelte";
+
 </script>
 
 <footer class="card-secondary">
-	<div class="logo">Logo</div>
-	<div class="links">Links</div>
-	<div class="contact">Contact</div>
+	<div class="logo">
+		<Logo />
+		<span>Meta Names</span>
+	</div>
+	<div class="links">
+		<a href="mailto:metanames@proton.me" target="_blank">Contact us</a>
+		<a href="https://x.com/metanames_" target="_blank">Twitter</a>
+	</div>
 </footer>
 
 <style>
+	.logo {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	a,
+	span {
+		color: var(--mdc-theme-on-primary);
+	}
+
 	footer {
 		display: flex;
 		flex-direction: row;
