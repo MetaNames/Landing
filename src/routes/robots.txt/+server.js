0c86f1920143f936ef3b11e92472ec7d2eb78bb7
Marco <marco@yeboster.com>
2023-12-17 16:22:59 +0100

    fix(seo): proper trim robots.txt

--- src/routes/robots.txt/+server.js
+++ src/routes/robots.txt/+server.js
@@ -8,7 +8,7 @@ export async function GET() {
     Allow: /
 
     Sitemap: ${websiteUrl}sitemap.xml
-		`.trim(),
+		`.split('\n').map(line => line.trim()).join('\n'),
 		{
 			headers: {
 				'Content-Type': 'text/plain'
