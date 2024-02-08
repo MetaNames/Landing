48c1b0eebc8250ddcac06b12211183156b18361d
Marco <marco@yeboster.com>
2024-02-08 15:31:27 +0100

    feat: increase cache to 20m

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -4,4 +4,4 @@ import Cache from 'timed-cache';
 
 export const sdkEnvironment = environment === 'test' ? Enviroment.testnet : Enviroment.mainnet;
 export const metaNamesSdk = new MetaNamesSdk(sdkEnvironment);
-export const cache = new Cache({ defaultTtl: 10 * 60 * 1000 }); // 10 minutes TTL
+export const cache = new Cache({ defaultTtl: 20 * 60 * 1000 }); // 20 minutes TTL
