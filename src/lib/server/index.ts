c31fd8623476781ba0e050b7bced5ec6a00c47ef
Marco <marco@yeboster.com>
2024-02-08 23:14:32 +0100

    refactor: remove unused cache

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,7 +1,5 @@
 import { environment } from "$lib";
 import { Enviroment, MetaNamesSdk } from "@metanames/sdk";
-import Cache from 'timed-cache';
 
 export const sdkEnvironment = environment === 'test' ? Enviroment.testnet : Enviroment.mainnet;
 export const metaNamesSdk = new MetaNamesSdk(sdkEnvironment);
-export const cache = new Cache({ defaultTtl: 20 * 60 * 1000 }); // 20 minutes TTL
