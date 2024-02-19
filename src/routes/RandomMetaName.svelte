ff466fbbd0fd4192b21e34fcf3ccfeebbb814c4b
Marco <marco@yeboster.com>
2024-02-19 17:48:58 +0100

    refactor(generator): shuffle names

--- src/routes/RandomMetaName.svelte
+++ src/routes/RandomMetaName.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-	import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
+	import {
+		uniqueNamesGenerator,
+		adjectives,
+		colors,
+		names,
+		starWars
+	} from 'unique-names-generator';
 
 	let rand = Math.floor(Math.random()) + 1;
+	const dictionaries = names.concat(adjectives, colors, starWars);
 	const customConfig = {
-		dictionaries: [adjectives, colors, animals],
+		dictionaries: [shuffleArray(dictionaries)],
 		separator: '-',
 		length: rand
 	};
+
+	function shuffleArray<T>(arr: T[]): T[] {
+		for (let i = arr.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[arr[i], arr[j]] = [arr[j], arr[i]];
+		}
+		return arr;
+	}
+
 	const generateRandomName = () => {
 		rand = Math.floor(Math.random()) + 1;
 		customConfig.length = rand;
-		return uniqueNamesGenerator(customConfig);
+		return uniqueNamesGenerator(customConfig).toLocaleLowerCase();
 	};
 
 	export let generatedName = generateRandomName();
