763a013f53f41da1272beb3d2e625560f9061e45
Marco <marco@yeboster.com>
2024-03-17 20:19:29 +0100

    refactor(api): use stats from app

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,5 +1,20 @@
-import { environment } from "$lib";
+import { environment, metaNamesAppUrl } from "$lib";
 import { Enviroment, MetaNamesSdk } from "@metanames/sdk";
 
 export const sdkEnvironment = environment === 'test' ? Enviroment.testnet : Enviroment.mainnet;
 export const metaNamesSdk = new MetaNamesSdk(sdkEnvironment);
+
+export interface DomainProjection {
+  name: string
+  createdAt: string
+}
+
+export interface DomainStats {
+  domainCount: number
+  ownerCount: number
+  recentDomains: DomainProjection[]
+}
+
+export const getMetaNamesStats = async () => {
+  return fetch(`${metaNamesAppUrl}/api/domains/stats`).then((res) => res.json()) as Promise<DomainStats>
+}
--- src/routes/api/stats/+server.ts
+++ src/routes/api/stats/+server.ts
@@ -1,18 +1,8 @@
-import { metaNamesSdk } from "$lib/server";
+import { getMetaNamesStats } from "$lib/server";
 import { json } from "@sveltejs/kit";
 
 export async function GET() {
-  const domainCount = await metaNamesSdk.domainRepository.count()
-  const ownerCount = await metaNamesSdk.domainRepository.getOwners().then(owners => owners.length)
-  const recentDomains = await metaNamesSdk.domainRepository.getAll()
-    .then((domains) => domains.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()).slice(0, 10)
-      .map((domain) => ({ name: domain.name, createdAt: domain.createdAt })));
-
-  const stats = {
-    ownerCount,
-    domainCount,
-    recentDomains
-  };
+  const stats = await getMetaNamesStats();
 
   return json(stats, { headers: { 'Cache-Control': 'max-age=600' } });
 }
